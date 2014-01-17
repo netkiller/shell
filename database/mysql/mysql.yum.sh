0141b8532462110f38dcf4e1c8acb3902b674b3d
Neo Chan <netkiller@msn.com>
2014-01-17 13:49:18 +0800

    Update mysql.yum.sh

--- database/mysql/mysql.yum.sh
+++ database/mysql/mysql.yum.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum localinstall http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
+yum localinstall -y http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
 yum install mysql-server -y
 chkconfig mysqld on
 service mysqld start
