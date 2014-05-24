1e02032852e5abca45ed84f170842ec346550e56
Neo Chen <netkiller@msn.com>
2014-05-24 19:16:20 +0800

    msgpack.sh safenet.sh

--- php/pecl/msgpack.sh
+++ php/pecl/msgpack.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-pecl install xdebug
+#pecl install msgpack
+pecl install  channel://pecl.php.net/msgpack-0.5.5  
 
-cat > /srv/php/etc/conf.d/xdebug.ini <<EOF
-[Zend Modules]
-zend_extension=xdebug.so
-;zend_extension_ts=xdebug.so
-
-[Xdebug]
-xdebug.profiler_enable = on
-xdebug.trace_output_dir = "/var/tmp/xdebug"
-xdebug.profiler_output_dir = "/var/tmp/xdebug"
+cat > /srv/php/etc/conf.d/msgpack.ini <<EOF
+extension=msgpack.so
 EOF
