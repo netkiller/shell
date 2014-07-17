3c4df61969fad8132f5cd32bd7ffb540a462db39
Neo Chen <netkiller@msn.com>
2014-07-17 12:00:41 +0800

    ubuntu docker

---
+++ docker/docker.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
