ab037bcf882a3fefbc26c9011c891e1fc3f3df8e
Neo Chen <netkiller@msn.com>
2014-09-22 17:39:30 +0800

    tomcat/init.d/tomcat

--- lang/java/server-jre.sh
+++ lang/java/server-jre.sh
@@ -8,7 +8,7 @@ ln -s /srv/jdk1.8.0_20 /srv/java
 
 cat >> /etc/profile.d/java.sh <<'EOF'
 export JAVA_HOME=/srv/java
-export JAVA_OPTS="-server -Xms512m -Xmx8192m  -XX:PermSize=64M -XX:MaxPermSize=512m"
+export JAVA_OPTS="-server -Xms512m -Xmx8192m"
 export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CATALINA_HOME/lib:
 export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$CATALINA_HOME/bin:
 EOF
--- web/tomcat/8.0.12.sh
+++ web/tomcat/8.0.12.sh
@@ -16,7 +16,7 @@ cp /srv/apache-tomcat/conf/server.xml{,.original}
 
 cat > /srv/apache-tomcat/bin/setenv.sh <<'EOF'
 export JAVA_HOME=/srv/java
-export JAVA_OPTS="-server -Xms512m -Xmx8192m  -XX:PermSize=64M -XX:MaxPermSize=512m"
+export JAVA_OPTS="-server -Xms512m -Xmx8192m"
 export CATALINA_HOME=/srv/apache-tomcat
 export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CATALINA_HOME/lib:/srv/IngrianJCE/lib/ext/IngrianNAE-5.1.1.jar
 export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$CATALINA_HOME/bin:
