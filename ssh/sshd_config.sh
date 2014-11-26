2124a6189b3957a8972379feab378084221ed0a9
Neo Chan <netkiller@msn.com>
2014-11-26 16:03:18 +0800

    Update sshd_config.sh

--- ssh/sshd_config.sh
+++ ssh/sshd_config.sh
@@ -1,6 +1,6 @@
 vim /etc/ssh/sshd_config <<EOF > /dev/null 2>&1
 :92,92s/#GSSAPIAuthentication no/GSSAPIAuthentication no/
 :93,93s/GSSAPIAuthentication yes/#GSSAPIAuthentication yes/
-:130,130s$#UseDNS yes$UseDNS no$
+:130,130s/#UseDNS yes/UseDNS no/
 :wq
 EOF
