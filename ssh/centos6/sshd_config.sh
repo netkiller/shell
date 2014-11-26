b70a107d2459891c5351a41bc5fc7ee1167c11cf
Neo Chan <netkiller@msn.com>
2014-11-26 16:00:27 +0800

    Update sshd_config.sh

--- ssh/centos6/sshd_config.sh
+++ ssh/centos6/sshd_config.sh
@@ -1,6 +1,6 @@
 vim /etc/ssh/sshd_config <<EOF > /dev/null 2>&1
 :80,80s/#GSSAPIAuthentication no/GSSAPIAuthentication no/
+:81,81s/GSSAPIAuthentication yes/#GSSAPIAuthentication yes/
 :122,122s$#UseDNS yes$UseDNS no$
-:80,80s/#GSSAPIAuthentication no/GSSAPIAuthentication no/
 :wq
 EOF
