3f5d4339b9582f4eb46e133d9d03363fdb62695f
Neo Chan <netkiller@msn.com>
2014-12-16 10:53:33 +0800

    Update git.sh

--- scm/git.sh
+++ scm/git.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ -z "$( egrep "CentOS|Redhat" /etc/issue)" ]; then
-        echo 'Only for Redhat or CentOS'
-        exit
-fi
+#if [ -z "$( egrep "CentOS|Redhat" /etc/issue)" ]; then
+#        echo 'Only for Redhat or CentOS'
+#        exit
+#fi
 
-yum install git -y
+if [ -f /usr/bin/yum ]; then
+        yum install git -y
+fi
