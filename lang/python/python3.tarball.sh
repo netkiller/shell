7f3c8168354db408d97cd4de8b63a2dbfea19b93
Neo Chan <netkiller@msn.com>
2014-12-16 11:50:05 +0800

    Update python3.tarball.sh

--- lang/python/python3.tarball.sh
+++ lang/python/python3.tarball.sh
@@ -1,10 +1,13 @@
-tar zxvf Python-3.4.0.tgz 
-cd Python-3.4.0
-./configure --prefix=/srv/python-3.4.0
+#!/bin/sh
+wget https://www.python.org/ftp/python/3.4.2/Python-3.4.2.tgz
+
+tar zxf Python-3.4.2.tgz 
+cd Python-3.4.2
+./configure --prefix=/srv/python-3.4.2
 make -j8
 make install
 
-ln -s /srv/python-3.4.0 /srv/python
+ln -s /srv/python-3.4.2 /srv/python
 
 cat >> ~/.bashrc <<EOF
 PATH=$PATH:/srv/python/bin:
