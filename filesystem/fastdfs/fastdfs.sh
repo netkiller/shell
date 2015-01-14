746dbb0cfc336d1997228ef61257869f0efcb710
netkiller <netkiller@msn.com>
2015-01-14 15:49:31 +0800

    nginx.centos6.sh

--- filesystem/fastdfs/fastdfs.sh
+++ filesystem/fastdfs/fastdfs.sh
@@ -6,3 +6,28 @@ cd FastDFS
 yum install -y libevent-devel
 ./make.sh 
 ./make.sh install
+
+cd
+
+cp /etc/fdfs/client.conf{,.original}
+cp /etc/fdfs/http.conf{,.original}
+cp /etc/fdfs/storage.conf{,.original}
+cp /etc/fdfs/tracker.conf{,.original}
+
+sed -i 's:base_path=/home/yuqing/fastdfs:base_path=/www/fastdfs:g' /etc/fdfs/client.conf 
+sed -i 's/tracker_server=192.168.0.197:22122/tracker_server=127.0.0.1:22122/g' /etc/fdfs/client.conf 
+sed -i 's:base_path=/home/yuqing/fastdfs:base_path=/www/fastdfs:g' /etc/fdfs/tracker.conf 
+sed -i 's:base_path=/home/yuqing/fastdfs:base_path=/www/fastdfs:g' /etc/fdfs/storage.conf 
+sed -i 's:store_path0=/home/yuqing/fastdfs:store_path0=/www/fastdfs:g' /etc/fdfs/storage.conf 
+sed -i 's/tracker_server=192.168.209.121:22122/tracker_server=127.0.0.1:22122/g' /etc/fdfs/storage.conf 
+
+##########################
+# Test
+##########################
+# fdfs_trackerd /etc/fdfs/tracker.conf
+# fdfs_storaged /etc/fdfs/storage.conf
+# fdfs_upload_file /etc/fdfs/client.conf /etc/issue
+# group1/M00/00/00/CvF8pVS2GziAcsCfAAAALysCLgI6731043
+# fdfs_test /etc/fdfs/client.conf upload /etc/centos-release
+##########################
+
---
+++ web/nginx/nginx.centos6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+
+yum install -y nginx
+chkconfig nginx on
+service nginx start
+
+cp /etc/nginx/nginx.conf{,.original}
+
+vim /etc/nginx/nginx.conf <<VIM > /dev/null 2>&1
+:%s/worker_processes  1;/worker_processes  8;/
+:%s/worker_connections  1024;/worker_connections  4096;/
+:%s/#gzip/server_tokens off;\r    gzip/
+:%s/#gzip/gzip/
+:wq
+VIM
