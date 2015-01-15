44c5c30e102ccda7235788a04a5aefa8432ed661
netkiller <netkiller@msn.com>
2015-01-15 15:57:57 +0800

    Tomcat

--- web/tomcat/7.0.57.sh
+++ web/tomcat/7.0.57.sh
@@ -12,6 +12,7 @@ tar zxf apache-tomcat-7.0.57.tar.gz
 mv apache-tomcat-7.0.57 /srv/
 ln -s /srv/apache-tomcat-7.0.57 /srv/apache-tomcat
 rm -rf /srv/apache-tomcat/webapps/*
+rm -rf /srv/apache-tomcat/logs/*
 
 cat > /srv/apache-tomcat/bin/setenv.sh <<'EOF'
 export JAVA_HOME=/srv/java
@@ -22,6 +23,15 @@ export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$CATALINA_HOME/bin:
 EOF
 
 cp /srv/apache-tomcat/conf/server.xml{,.original}
+
+vim /srv/apache-tomcat/conf/server.xml <<VIM > /dev/null 2>&1
+:73,73s:/>:maxThreads="4096" enableLookups="false" compression="on" compressionMinSize="2048" compressableMimeType="text/html,text/xml,text/javascript,text/css,text/plain,,application/octet-stream" />:
+:126,126s/true/false/g
+:93s/</<!-- </
+:93s/>/> -->/
+:wq
+VIM
+
 
 groupadd -g 80 www
 adduser -o --home /www --uid 80 --gid 80 -c "Web Application" www
---
+++ web/tomcat/ROOT/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<HTML>
+<HEAD>
+<TITLE>HelloWorld!</TITLE>
+</HEAD>
+<BODY>
+<%
+        out.println("<h1>Hello World!</h1>");
+%>
+</BODY>
+</HTML>
