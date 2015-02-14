f5148eae14d7a48105ccfbd8367b402623113452
Neo Chan <netkiller@msn.com>
2015-02-14 15:59:38 +0800

    Update install.sh

--- monitor/zabbix/install.sh
+++ monitor/zabbix/install.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-yum localinstall http://repo.zabbix.com/zabbix/2.4/rhel/7/x86_64/zabbix-release-2.4-1.el7.noarch.rpm
+yum localinstall -y http://repo.zabbix.com/zabbix/2.4/rhel/7/x86_64/zabbix-release-2.4-1.el7.noarch.rpm
+
+yum install -y zabbix-server-mysql zabbix-web-mysql
+
+cd /usr/share/doc/zabbix-server-mysql-2.4.0/create/
+
+mysql -uzabbix -p zabbix < schema.sql
+mysql -uzabbix -p zabbix < images.sql
+mysql -uzabbix -p zabbix < data.sql
+
+cp /etc/zabbix/zabbix_server.conf{,.original}
+vim /etc/zabbix/zabbix_server.conf <<EOF > /dev/null 2>&1
+:%s/# DBPassword=/DBPassword=your_password/
+:wq
+EOF
+
+systemctl start zabbix-server
+systemctl restart httpd
