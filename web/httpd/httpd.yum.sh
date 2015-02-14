d317b6f37dfa957633376a05615fa87925894e9b
Neo Chan <netkiller@msn.com>
2015-02-14 16:02:30 +0800

    Update httpd.yum.sh

--- web/httpd/httpd.yum.sh
+++ web/httpd/httpd.yum.sh
@@ -4,6 +4,8 @@ yum install -y httpd php
 systemctl enable httpd
 systemctl start httpd
 
+cp /etc/php.ini{,.original}
+
 firewall-cmd --permanent --zone=public --add-service=http 
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
