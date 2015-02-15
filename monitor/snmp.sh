1b0b1bfdb5e81072a03413b24bd911fd046e82a5
netkiller <netkiller@msn.com>
2015-02-15 10:07:14 +0800

    snmp.sh

--- monitor/snmp.sh
+++ monitor/snmp.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [ -z "$( egrep "CentOS|Redhat" /etc/issue)" ]; then
-        echo 'Only for Redhat or CentOS'
-        exit
-fi
-
 yum install net-snmp -y
-vi /etc/snmp/snmpd.conf <<VIM > /dev/null 2>&1
+cp /etc/snmp/snmpd.conf{,.original}
+vim /etc/snmp/snmpd.conf <<VIM > /dev/null 2>&1
 :62,62s/systemview/all/
 :85,85s/^#//
+:162,162s/syslocation Unknown/syslocation Neo/
+:163,163s/syscontact Root <root@localhost>/syscontact Neo <netkiller@msn.com>/
 :wq
 VIM
 service snmpd start
