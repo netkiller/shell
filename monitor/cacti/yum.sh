e87b0c8bea363c57367144eee66c29dbbb12d915
netkiller <netkiller@msn.com>
2015-03-04 11:23:54 +0800

    comment snmp v1

--- monitor/cacti/yum.sh
+++ monitor/cacti/yum.sh
@@ -23,15 +23,3 @@ VIM
 sed -i 's/^#//' /etc/cron.d/cacti
 
 service httpd reload
-
-cd /usr/local/src
-wget http://docs.cacti.net/_media/plugin:monitor-v1.3-1.tgz
-wget http://docs.cacti.net/_media/plugin:errorimage-v0.2-1.tgz
-wget http://docs.cacti.net/_media/plugin:discovery-v1.5-1.tgz
-
-
-mv plugin\:monitor-v1.3-1.tgz monitor-v1.3-1.tgz
-mv plugin\:errorimage-v0.2-1.tgz errorimage-v0.2-1.tgz 
-
-tar zxf monitor-v1.3-1.tgz -C /usr/share/cacti/plugins
-tar zxf errorimage-v0.2-1.tgz -C /usr/share/cacti/plugins
--- monitor/snmp.sh
+++ monitor/snmp.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 yum install net-snmp -y
+
 cp /etc/snmp/snmpd.conf{,.original}
 vim /etc/snmp/snmpd.conf <<VIM > /dev/null 2>&1
+:47,47s/^/#/
 :62,62s/systemview/all/
 :85,85s/^#//
 :162,162s/syslocation Unknown/syslocation Neo/
 :163,163s/syscontact Root <root@localhost>/syscontact Neo <netkiller@msn.com>/
 :wq
 VIM
+
 service snmpd start
 chkconfig snmpd on
