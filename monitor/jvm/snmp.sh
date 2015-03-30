9b964fc5a9d384891febec308a5d75b96c8c452b
netkiller <netkiller@msn.com>
2015-03-30 17:26:31 +0800

    jvm/snmp.sh

---
+++ monitor/jvm/snmp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cp $JAVA_HOME/jre/lib/management/snmp.acl.template $JAVA_HOME/jre/lib/management/snmp.acl
+
+cat >> $JAVA_HOME/jre/lib/management/snmp.acl <<EOD
+
+acl = {
+ {
+   communities = public
+   access = read-only
+   managers = localhost
+ }
+}
+EOD
+
+
+test -f /etc/snmp/snmp.local.conf && cp /etc/snmp/snmp.local.conf{,.original}
+
+cat >> /etc/snmp/snmp.local.conf <<EOD
+
+#JVM-MANAGEMENT-MIB
+proxy -v 2c -c public localhost:9004 .1.3.6.1.4.1.42
+EOD
