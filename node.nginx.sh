99a35726c0e697da4bc7038f5959b388b4484cbd
Neo Chan <netkiller@msn.com>
2015-05-27 16:00:05 +0800

    Update node.nginx.sh

--- node.nginx.sh
+++ node.nginx.sh
@@ -1,13 +1,9 @@
-Automatic installation web server
-=====
-
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/os/centos7.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/os/iptables.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/gcc/gcc.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/web/nginx/nginx.centos7.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/5.6.9-centos7.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/redis.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/pthreads.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/amqp.sh | bash
-  curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/phalcon.sh | bash
-
+curl -s https://raw.githubusercontent.com/oscm/shell/master/os/centos7.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/os/iptables.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/gcc/gcc.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/nginx/nginx.centos7.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/5.6.9-centos7.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/redis.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/pthreads.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/amqp.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/php/pecl/phalcon.sh | bash
