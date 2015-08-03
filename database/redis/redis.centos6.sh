cc7cf6caff7d079b424d2c15de2469cc15f9573b
netkiller <netkiller@msn.com>
2015-08-03 15:09:35 +0800

    redis.sh

--- database/redis/redis.centos6.sh
+++ database/redis/redis.centos6.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ -z "$( egrep "CentOS|Redhat" /etc/issue)" ]; then
+        echo 'Only for Redhat or CentOS'
+        exit
+fi
+
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+
 yum install redis -y
+
+
 
 cp /etc/redis.conf{,.original}
 sed -i 's/daemonize no/daemonize yes/' /etc/redis.conf 
@@ -18,8 +27,8 @@ EOF
 
 sysctl vm.overcommit_memory=1
 
-systemctl enable redis
-systemctl start redis
+chkconfig redis on
+service redis start
 
 iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 6379 -j ACCEPT
 service iptables save
