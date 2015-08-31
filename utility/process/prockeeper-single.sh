7998b84efc9d956994685a3993b8200a9ea44928
Neo Chan <netkiller@msn.com>
2015-08-31 15:59:55 +0800

    Create prockeeper-single.sh

---
+++ utility/process/prockeeper-single.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+LOGFILE=/var/log/$(basename $0 .sh).log
+PATTERN="php-fpm"
+RECOVERY="/etc/init.d/php-fpm restart"
+
+while true
+do
+    TIMEPOINT=$(date -d "today" +"%Y-%m-%d_%H:%M:%S")
+    PROC=$(pgrep -o -f ${PATTERN})
+    #echo ${PROC}
+    if [ -z "${PROC}" ]; then
+		${RECOVERY} >> $LOGFILE
+		echo "[${TIMEPOINT}] ${PATTERN} ${RECOVERY}" >> $LOGFILE
+    #else
+        #echo "[${TIMEPOINT}] ${PATTERN} ${PROC}" >> $LOGFILE
+    fi
+sleep 5
+done &
