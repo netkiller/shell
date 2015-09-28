30ed952bfae8956b1b276ef0020099960f62189b
netkiller <netkiller@msn.com>
2015-09-28 14:37:16 +0800

    lang/php/7.0.0.sh

--- lang/php/7.0.0.sh
+++ lang/php/7.0.0.sh
@@ -14,17 +14,16 @@ yum install -y http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-centos94-
 yum install -y postgresql94-devel
 
 cd /usr/local/src/
-#wget http://cn2.php.net/distributions/php-7.0.0alpha1.tar.gz
-wget https://downloads.php.net/~ab/php-7.0.0alpha1alpha1.tar.gz
+wget https://downloads.php.net/~ab/php-7.0.0RC3.tar.gz
 
-if [ -s php-7.0.0alpha1alpha1.tar.gz ]; then
+if [ -s php-7.0.0RC3.tar.gz ]; then
 
-tar zxf php-7.0.0alpha1alpha1.tar.gz
-cd php-7.0.0alpha1
+tar zxf php-7.0.0RC3.tar.gz
+cd php-7.0.0RC3
 
-./configure --prefix=/srv/php-7.0.0alpha1 \
---with-config-file-path=/srv/php-7.0.0alpha1/etc \
---with-config-file-scan-dir=/srv/php-7.0.0alpha1/etc/conf.d \
+./configure --prefix=/srv/php-7.0.0RC3 \
+--with-config-file-path=/srv/php-7.0.0RC3/etc \
+--with-config-file-scan-dir=/srv/php-7.0.0RC3/etc/conf.d \
 --enable-fpm \
 --enable-opcache \
 --with-fpm-user=www \
@@ -86,48 +85,54 @@ fi
 
 [[ $? -ne 0 ]] && echo "Error: make install" &&  exit $?
 
-strip /srv/php-7.0.0alpha1/bin/php
-strip /srv/php-7.0.0alpha1/sbin/php-fpm 
-
-mkdir -p /srv/php-7.0.0alpha1/etc/conf.d
-mkdir -p /srv/php-7.0.0alpha1/etc/fpm.d
-cp /srv/php-7.0.0alpha1/etc/pear.conf{,.original}
-cp php.ini-* /srv/php-7.0.0alpha1/etc/
-cp /srv/php-7.0.0alpha1/etc/php.ini-production /srv/php-7.0.0alpha1/etc/php.ini
-cp /srv/php-7.0.0alpha1/etc/php.ini-development /srv/php-7.0.0alpha1/etc/php-cli.ini
-cp /srv/php-7.0.0alpha1/etc/php-fpm.conf.default /srv/php-7.0.0alpha1/etc/php-fpm.conf
-
-/etc/init.d/php-fpm stop
-#\cp sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
-yes|cp sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
-chmod +x /etc/init.d/php-fpm
-chkconfig --add php-fpm
-chkconfig php-fpm on
-
 rm -f /srv/php
-ln -s /srv/php-7.0.0alpha1/ /srv/php
+ln -s /srv/php-7.0.0RC3/ /srv/php
 
-vim /srv/php-7.0.0alpha1/etc/php-fpm.conf <<end > /dev/null 2>&1
-:25,25s/;//
-:32,32s/;//
-:93,93s/;rlimit_files = 1024/rlimit_files = 65536/
-:235,235s/pm.max_children = 5/pm.max_children = 2048/
-:240,240s/pm.start_servers = 2/pm.start_servers = 8/
-:245,245s/pm.min_spare_servers = 1/pm.min_spare_servers = 8/
-:250,250s/pm.max_spare_servers = 3/pm.max_spare_servers = 16/
-:261,261s/;pm.max_requests = 500/pm.max_requests = 1024/
-:360,360s/;pm.status_path/pm.status_path/
-:372,372s/;ping.path/ping.path/
-:377,377s/;ping.response/ping.response/
-:454,454s/;request_terminate_timeout = 0/request_terminate_timeout = 30s/
-:458,458s/;rlimit_files = 1024/rlimit_files = 40960/
+strip /srv/php-7.0.0RC3/bin/php
+strip /srv/php-7.0.0RC3/sbin/php-fpm 
+
+mkdir -p /srv/php-7.0.0RC3/etc/conf.d
+mkdir -p /srv/php-7.0.0RC3/etc/fpm.d
+cp /srv/php-7.0.0RC3/etc/pear.conf{,.original}
+cp php.ini-* /srv/php-7.0.0RC3/etc/
+cp /srv/php-7.0.0RC3/etc/php.ini-production /srv/php-7.0.0RC3/etc/php.ini
+cp /srv/php-7.0.0RC3/etc/php.ini-production /srv/php-7.0.0RC3/etc/php-cli.ini
+cp /srv/php-7.0.0RC3/etc/php-fpm.conf.default /srv/php-7.0.0RC3/etc/php-fpm.conf
+cp /srv/php-7.0.0RC3/etc/php-fpm.d/www.conf.default /srv/php-7.0.0RC3/etc/php-fpm.d/www.conf
+
+yes|cp ./sapi/fpm/php-fpm.service /etc/systemd/system/php-fpm.service 
+sed -i 's:${prefix}:/srv/php:g' /etc/systemd/system/php-fpm.service
+sed -i 's:${exec_prefix}:/srv/php:g' /etc/systemd/system/php-fpm.service
+
+systemctl enable php-fpm
+
+
+
+vim /srv/php-7.0.0RC3/etc/php-fpm.conf <<end > /dev/null 2>&1
+:17,17s/;pid/pid/
+:24,24s/;error_log/error_log/
+:85,85s/;rlimit_files = 1024/rlimit_files = 65536/
+:wq
+end
+
+vim /srv/php-7.0.0RC3/etc/php-fpm.d/www.conf <<end > /dev/null 2>&1
+:107,107s/pm.max_children = 5/pm.max_children = 2048/
+:112,112s/pm.start_servers = 2/pm.start_servers = 8/
+:117,117s/pm.min_spare_servers = 1/pm.min_spare_servers = 8/
+:122,122s/pm.max_spare_servers = 3/pm.max_spare_servers = 16/
+:133,133s/;pm.max_requests = 500/pm.max_requests = 1024/
+:232,232s/;pm.status_path/pm.status_path/
+:244,244s/;ping.path/ping.path/
+:249,249s/;ping.response/ping.response/
+:330,330s/;request_terminate_timeout = 0/request_terminate_timeout = 30s/
+:334,334s/;rlimit_files = 1024/rlimit_files = 40960/
 :wq
 end
 
 #:15,15s/;//
 
-vim /srv/php-7.0.0alpha1/etc/php.ini <<EOF > /dev/null 2>&1
-:298,298s$;open_basedir =$open_basedir = /www/:/tmp/:/var/tmp/:/srv/php-7.0.0alpha1/lib/php/:/srv/php-7.0.0alpha1/bin/$
+vim /srv/php-7.0.0RC3/etc/php.ini <<EOF > /dev/null 2>&1
+:298,298s$;open_basedir =$open_basedir = /www/:/tmp/:/var/tmp/:/srv/php-7.0.0RC3/lib/php/:/srv/php-7.0.0RC3/bin/$
 :303,303s/disable_functions =/disable_functions = ini_set,set_time_limit,set_include_path,passthru,exec,system,chroot,scandir,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,fsocket/
 :363,363s/expose_php = On/expose_php = Off/
 :393,393s/memory_limit = 128M/memory_limit = 32M/
@@ -139,14 +144,14 @@ vim /srv/php-7.0.0alpha1/etc/php.ini <<EOF > /dev/null 2>&1
 EOF
 
 #s/max_execution_time = 30/max_execution_time = 300/g
-#:706,706s!;include_path = ".:/php/includes"!include_path = ".:/srv/php-7.0.0alpha1/lib/php:/srv/php-7.0.0alpha1/share"!
-#:728,728s!; extension_dir = "./"!extension_dir = "./:/srv/php-7.0.0alpha1/lib/php/extensions:/srv/php-7.0.0alpha1/lib/php/extensions/no-debug-non-zts-20121212"!
+#:706,706s!;include_path = ".:/php/includes"!include_path = ".:/srv/php-7.0.0RC3/lib/php:/srv/php-7.0.0RC3/share"!
+#:728,728s!; extension_dir = "./"!extension_dir = "./:/srv/php-7.0.0RC3/lib/php/extensions:/srv/php-7.0.0RC3/lib/php/extensions/no-debug-non-zts-20121212"!
 #:804,804s/upload_max_filesize = 2M/upload_max_filesize = 3M/
 
-vim /srv/php-7.0.0alpha1/etc/php.cli.ini <<EOF > /dev/null 2>&1
-:393,393s/memory_limit = 128M/memory_limit = 4G/
-:572,572s/;error_log/error_log/
-:927,927s:;date.timezone =:date.timezone = Asia/Hong_Kong:
+vim /srv/php-7.0.0RC3/etc/php-cli.ini <<EOF > /dev/null 2>&1
+:389,389s/memory_limit = 128M/memory_limit = 4G/
+:568,568s:;error_log = php_errors.log:error_log = /var/tmp/php_errors.log:
+:913,913s:;date.timezone =:date.timezone = Asia/Hong_Kong:
 :wq
 EOF
 
@@ -166,4 +171,4 @@ EOF
 
 source /etc/profile.d/php.sh
 
-/etc/init.d/php-fpm start
+systemctl start php-fpm
