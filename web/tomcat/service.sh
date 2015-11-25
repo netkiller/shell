43cdcd62857bfe238871d2839b6aeaeff915f906
netkiller <netkiller@msn.com>
2015-11-25 17:50:17 +0800

    tomcat/service.sh

--- web/tomcat/service.sh
+++ web/tomcat/service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/init.d/tomcat -O /etc/init.d/tomcat
+wget -q --no-check-certificate https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/init.d/tomcat -O /etc/init.d/tomcat
 chmod +x /etc/init.d/tomcat
 chkconfig tomcat on
