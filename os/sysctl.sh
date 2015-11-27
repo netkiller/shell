08c5d69972c737549b5af5369b22409ddecad2e3
Neo Chan <netkiller@msn.com>
2015-11-27 13:02:34 +0800

    Create sysctl.sh

---
+++ os/sysctl.sh
@@ -0,0 +1,15 @@
+cat >> /etc/sysctl.conf <<EOF
+
+net.ipv4.ip_local_port_range = 1024 65500
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_keepalive_time = 1800
+net.core.netdev_max_backlog=3000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 4096
+net.core.somaxconn = 1024
+EOF
+#net.ipv4.tcp_syncookies = 1
+#net.ipv4.tcp_fin_timeout = 60
+
+sysctl -p
