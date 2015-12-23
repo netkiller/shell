7e99aec082f6bd9928b40725351b003d6c1a2f30
netkiller <netkiller@msn.com>
2015-12-23 10:18:16 +0800

    tomcat

--- cloud/aliyun/tomcat7.sh
+++ cloud/aliyun/tomcat7.sh
@@ -1,5 +1,5 @@
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/user/www.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/apache-tomcat-7.0.67.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/setenv.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/profile.d.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/service.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/7.0/apache-tomcat-7.0.67.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/7.0/setenv.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/7.0/profile.d.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/7.0/service.sh | bash
--- cloud/aliyun/tomcat8.sh
+++ cloud/aliyun/tomcat8.sh
@@ -1,5 +1,9 @@
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/user/www.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/apache-tomcat-7.0.67.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/apache-tomcat-8.0.26.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/tomcat-native-1.2.3.sh  | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/tomcat-native.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/setenv.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/profile.d.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/service.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/systemctl.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/firewall.sh  | bash
+
