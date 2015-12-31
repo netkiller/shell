e8fcb0be8c9025661304c18de77f64afc645334d
netkiller <netkiller@msn.com>
2015-12-31 17:44:32 +0800

    mysql/performance.sh

---
+++ database/mysql/performance.sh
@@ -0,0 +1,8 @@
+sed -i 's/default/default,noatime,nodiratime,nobarrier/' /etc/fstab
+echo "vm.swappiness = 0" >>/etc/sysctl.conf
+#cat >> /etc/rc.local <<EOF
+
+#echo deadline >/sys/block/sda/queue/scheduler
+#EOF
+#echo deadline >/sys/block/sda/queue/scheduler
+
--- os/centos7.sh
+++ os/centos7.sh
@@ -2,11 +2,12 @@
 
 #yum install -y openssh-clients
 
-yum localinstall -y http://ftp.cuhk.edu.hk/pub/linux/fedora-epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum localinstall -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+#http://ftp.cuhk.edu.hk/pub/linux/fedora-epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 
 yum remove -y dhclient dhcp-*
 yum install -y telnet wget rsync vim-enhanced
-yum install -y net-tools
+yum install -y net-tools bind-utils
 
 yum update -y
 
