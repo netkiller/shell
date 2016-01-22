81df468f8083035cd3e917feb7e05857800f1f3e
netkiller <netkiller@msn.com>
2016-01-22 17:35:23 +0800

    php-5.6.17.sh

--- lang/php/pecl/pthreads.sh
+++ lang/php/pecl/pthreads.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# php-5.6.17
+#pecl install pthreads-2.0.10
+
+
 pecl install pthreads
 
 cat > /srv/php/etc/conf.d/pthreads.ini <<EOF
---
+++ lang/php/php-5.6.17-default.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -f /srv/php
+ln -s /srv/php-5.6.17/ /srv/php
