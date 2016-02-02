d51351aaecee997a87d679a06225d8df3b83f372
netkiller <netkiller@msn.com>
2016-02-02 10:01:44 +0800

    docker.repo.sh

---
+++ virtualization/docker/docker.repo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
