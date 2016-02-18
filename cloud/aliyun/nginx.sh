ab6645127541a975a9337d5345128c876459fc5d
Neo Chan <netkiller@msn.com>
2016-02-18 16:27:02 +0800

    Update nginx.sh

--- cloud/aliyun/nginx.sh
+++ cloud/aliyun/nginx.sh
@@ -1,4 +1,4 @@
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/centos7.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/ntpd/ntp.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/user/www.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/nginx/nginx.centos7.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/nginx/nginx.sh | bash
