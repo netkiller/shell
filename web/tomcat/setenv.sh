00c2e4c1c8b79940d2d8d8adf2c5fd79e10e9e78
netkiller <netkiller@msn.com>
2016-05-17 12:01:15 +0800

    java -Djava.security.egd=file:/dev/./urandom

--- web/tomcat/setenv.sh
+++ web/tomcat/setenv.sh
@@ -2,11 +2,11 @@
 cat > /srv/apache-tomcat/bin/setenv.sh <<'EOF'
 export JRE_HOME=/srv/java
 export JAVA_HOME=/srv/java
-export JAVA_OPTS="-server -Xms2048m -Xmx8192m"
+export JAVA_OPTS="-server -Xms2048m -Xmx8192m -Djava.security.egd=file:/dev/./urandom"
 
-export CATALINA_HOME=/srv/apache-tomcat
-export LD_LIBRARY_PATH=/srv/apache-tomcat/lib
-export CATALINA_OPTS=" -Djava.awt.headless=true -Djava.library.path=$LD_LIBRARY_PATH"
+#export CATALINA_HOME=/srv/apache-tomcat
+#export LD_LIBRARY_PATH=/srv/apache-tomcat/lib
+#export CATALINA_OPTS=" -Djava.awt.headless=true -Djava.library.path=$LD_LIBRARY_PATH"
 #export CATALINA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9012 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true"
 #export CATALINA_TMPDIR=/var/tmp
 
