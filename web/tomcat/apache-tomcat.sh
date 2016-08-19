f1136186466d741f9fce633d5418937d5e2e5fe2
netkiller <netkiller@msn.com>
2016-08-19 11:56:14 +0800

    Test tools

--- web/tomcat/apache-tomcat.sh
+++ web/tomcat/apache-tomcat.sh
@@ -3,4 +3,4 @@ curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/apache-to
 curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/apache-tomcat-default.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/setenv.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/systemctl.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/logrotate.d/compress
+curl -s https://raw.githubusercontent.com/oscm/shell/master/web/tomcat/logrotate.d/compress | bash
