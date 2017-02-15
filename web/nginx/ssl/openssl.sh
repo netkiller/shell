5fcac415f41d2612647e07b83bd4706f0225da91
netkiller <netkiller@msn.com>
2017-02-15 15:01:06 +0800

    http2.sh

--- web/nginx/ssl/openssl.sh
+++ web/nginx/ssl/openssl.sh
@@ -1,4 +1,4 @@
 
 mkdir /etc/nginx/ssl
 cd /etc/nginx/ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/api.netkiller.cn.key -out /etc/nginx/ssl/api.netkiller.cn.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/www.netkiller.cn.key -out /etc/nginx/ssl/www.netkiller.cn.crt
