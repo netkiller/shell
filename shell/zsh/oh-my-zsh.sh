719d3360d978846999b6ea54ed72e551620ce627
Neo Chen <Neo Chen netkiller@msn.com>
2017-03-27 18:48:50 +0800

    zsh/oh-my-zsh.sh

---
+++ shell/zsh/oh-my-zsh.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+###########################################
+# http://netkiller.github.io
+# Neo chen <netkiller@msn.com>
+###########################################
+yum install -y zsh
+###########################################
+$ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
