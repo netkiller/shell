fda0181e2e023d43a0776cfe20741d3005e270e1
Neo Chen <Neo Chen netkiller@msn.com>
2017-03-30 14:25:43 +0800

    zsh/oh-my-zsh.sh

--- shell/zsh/oh-my-zsh.sh
+++ shell/zsh/oh-my-zsh.sh
@@ -3,8 +3,23 @@
 # http://netkiller.github.io
 # Neo chen <netkiller@msn.com>
 ###########################################
-yum install -y zsh
+if [ $(id -u) = "0" ]; then
+	yum install -y zsh
+fi
 ###########################################
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
+
+cat >> ~/.zshrc <<EOF
+
+if [ -f ~/.zsh_aliases ]; then
+    . ~/.zsh_aliases
+fi
+
+bindkey '\e[1~' beginning-of-line
+bindkey '\e[4~' end-of-line
+
+EOF
 
 chsh -s /bin/zsh
