f608b8170b2a77d7a362f4919268831b0151b507
Neo Chen <Neo Chen netkiller@msn.com>
2017-04-11 14:32:53 +0800

    nginx-1.11.13.sh

--- web/nginx/source/nginx-1.11.13.sh
+++ web/nginx/source/nginx-1.11.13.sh
@@ -15,6 +15,7 @@ make depend
 make -j8
 make install
 
+rm -f /srv/openssl
 ln -s /srv/openssl-1.1.0e/ /srv/openssl
 
 cd /usr/local/src/
@@ -23,11 +24,13 @@ wget http://nginx.org/download/nginx-1.11.13.tar.gz
 tar zxvf nginx-1.11.13.tar.gz
 cd nginx-1.11.13
 ./configure --prefix=/srv/nginx-1.11.13 \
---http-client-body-temp-path=/srv/nginx-1.11.13/var/cache/nginx/client_temp \
---http-proxy-temp-path=/srv/nginx-1.11.13/var/cache/nginx/proxy_temp \
---http-fastcgi-temp-path=/srv/nginx-1.11.13/var/cache/nginx/fastcgi_temp \
---http-uwsgi-temp-path=/srv/nginx-1.11.13/var/cache/nginx/uwsgi_temp \
---http-scgi-temp-path=/srv/nginx-1.11.13/var/cache/nginx/scgi_temp \
+--pid-path=/var/run/nginx.pid \
+--lock-path=/var/run/nginx.lock \
+--http-client-body-temp-path=/var/cache/nginx/client_temp \
+--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
 --user=nginx \
 --group=nginx \
 --with-openssl=../openssl-1.1.0e \
@@ -51,49 +54,9 @@ cd nginx-1.11.13
 --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic -fPIC' \
 --with-ld-opt='-Wl,-z,relro -Wl,-z,now -pie'
 
-#./configure --prefix=/srv/nginx-1.11.13 \
-# --sbin-path=/usr/sbin/nginx \
-# --modules-path=/usr/lib64/nginx/modules \
-# --conf-path=/etc/nginx/nginx.conf \
-#--error-log-path=/var/log/nginx/error.log \
-#--http-log-path=/var/log/nginx/access.log \
-#--pid-path=/var/run/nginx.pid \
-#--lock-path=/var/run/nginx.lock \
-#--http-client-body-temp-path=/var/cache/nginx/client_temp \
-#--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
-#--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
-#--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
-#--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
-#--user=nginx \
-#--group=nginx \
-#--with-file-aio \ 
-#--with-threads \
-#--with-ipv6 \
-#--with-http_addition_module \
-#--with-http_auth_request_module \
-#--with-http_dav_module \
-#--with-http_flv_module \
-#--with-http_gunzip_module \
-#--with-http_gzip_static_module \
-#--with-http_mp4_module \
-#--with-http_random_index_module \
-#--with-http_realip_module \
-#--with-http_secure_link_module \
-#--with-http_slice_module \
-#--with-http_ssl_module \
-#--with-http_stub_status_module \
-#--with-http_sub_module \
-#--with-http_v2_module \
-#--with-mail \
-#--with-mail_ssl_module \
-#--with-stream \
-#--with-stream_ssl_module \
-#--with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic -fPIC' \
-#--with-ld-opt='-Wl,-z,relro -Wl,-z,now -pie'
-#--add-module=../fastdfs-nginx-module/src
-
 make -j4  && make install
 
+rm -f /srv/nginx
 ln -s /srv/nginx-1.11.13 /srv/nginx
 
 strip /srv/nginx-1.11.13/sbin/nginx
@@ -108,8 +71,8 @@ vim /srv/nginx-1.11.13/conf/nginx.conf <<VIM > /dev/null 2>&1
 :wq
 VIM
 
-mkdir -p /srv/nginx-1.11.13/var/cache/nginx/client_temp
-chown nginx:nginx -R /srv/nginx-1.11.13/var/cache/
+mkdir -p /var/cache/nginx/
+chown nginx:nginx -R /var/cache/nginx/
 
 wget -q https://raw.githubusercontent.com/oscm/shell/master/web/nginx/source/nginx.service -O /usr/lib/systemd/system/nginx.service
 systemctl enable nginx
