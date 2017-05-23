03793be721af217197f78ceaee51da6875491f03
Neo Chen <Neo Chen netkiller@msn.com>
2017-05-23 11:43:46 +0800

    java

--- lang/java/openjdk/java-1.8.0-openjdk.sh
+++ lang/java/openjdk/java-1.8.0-openjdk.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 yum install -y java-1.8.0-openjdk
+
+cat >> /etc/profile.d/java.sh <<'EOF'
+export JAVA_HOME=/usr/java/default
+export JAVA_OPTS="-server -Xms2048m -Xmx4096m -Djava.io.tmpdir=/tmp -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF8  -Duser.timezone=GMT+08"
+export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:.
+export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:
+EOF
+
+source /etc/profile.d/java.sh
