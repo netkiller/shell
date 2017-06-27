5650ae60c457e26b14f47cf6c65dd6b54ad97f6b
Neo Chan <netkiller@msn.com>
2017-06-27 16:54:20 +0800

    Create apache-hive-2.1.1.sh

---
+++ database/apache-hive/apache-hive-2.1.1.sh
@@ -0,0 +1,6 @@
+cd /usr/local/src
+wget http://mirrors.hust.edu.cn/apache/hive/stable-2/apache-hive-2.1.1-bin.tar.gz
+
+tar zxf apache-hive-2.1.1-bin.tar.gz
+mv apache-hive-2.1.1-bin /srv/apache-hive-2.1.1
+ln -s /srv/apache-hive-2.1.1/ /srv/apache-hive
