19aff8bf981d4ee77aee871af8f429e3a052b38b
Neo Chan <netkiller@msn.com>
2017-06-27 17:59:13 +0800

    rename hbase to apache-hbase

--- database/apache-hbase/apache-hbase-1.2.6.sh
+++ database/apache-hbase/apache-hbase-1.2.6.sh
@@ -3,5 +3,5 @@ wget http://mirrors.hust.edu.cn/apache/hbase/stable/hbase-1.2.6-bin.tar.gz
 
 tar zxf hbase-1.2.6-bin.tar.gz
 cp hbase-1.2.6/conf/hbase-site.xml{,.original}
-mv hbase-1.2.6 /srv/
-ln -s /srv/hbase-1.2.6 /srv/hbase
+mv hbase-1.2.6 /srv/apache-hbase-1.2.6
+ln -s /srv/apache-hbase-1.2.6 /srv/apache-hbase
