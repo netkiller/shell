814b2caf5f1caf46df37a870a2ac72adc2bb60bc
Neo Chan <netkiller@msn.com>
2017-07-31 10:28:21 +0800

    elasticsearch

--- database/mysql/5.7/mysql-connector-java.sh
+++ database/mysql/5.7/mysql-connector-java.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+if [ -z $(rpm -qa mysql57-community-release) ]; then
+    curl -s https://raw.githubusercontent.com/oscm/shell/master/database/mysql/5.7/mysql57-community-release-el7-7.sh | bash
+fi
+
 yum install -y mysql-connector-java
 ls /usr/share/java/mysql-connector-java.jar
+
+
--- os/personalise.netkiller.sh
+++ os/personalise.netkiller.sh
@@ -1,2 +1,2 @@
-
+curl -s https://raw.githubusercontent.com/oscm/shell/master/os/personalise | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/ssh/banner.sh | bash
