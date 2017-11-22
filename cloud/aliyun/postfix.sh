fdff01e014b1745b31939254cf35dbdb91e76a6d
Neo Chan <netkiller@msn.com>
2017-11-22 14:23:22 +0800

    修改脚本

--- cloud/aliyun/postfix.sh
+++ cloud/aliyun/postfix.sh
@@ -1,4 +1,5 @@
-curl -s https://raw.githubusercontent.com/oscm/shell/master/os/centos7.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/os/epel-release.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/os/etc.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/selinux.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/iptables/iptables.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/ntpd/ntpdate.sh | bash
