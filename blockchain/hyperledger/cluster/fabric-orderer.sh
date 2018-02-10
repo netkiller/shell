1dfd6dd66c69efcbc11668c9943a31d2f0d6980c
Neo Chan <netkiller@msn.com>
2018-02-10 15:24:11 +0800

    区块链

---
+++ blockchain/hyperledger/cluster/fabric-orderer.sh
@@ -0,0 +1,2 @@
+docker pull hyperledger/fabric-orderer:x86_64-1.0.5
+docker tag hyperledger/fabric-orderer:x86_64-1.0.5 hyperledger/fabric-orderer
---
+++ blockchain/hyperledger/cluster/fabric-peer.sh
@@ -0,0 +1,2 @@
+docker pull hyperledger/fabric-peer:x86_64-1.0.5
+docker tag hyperledger/fabric-peer:x86_64-1.0.5 hyperledger/fabric-peer
