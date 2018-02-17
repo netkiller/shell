5ced53f3e4f14de57a894c6a371462283d4479aa
Neo Chan <netkiller@msn.com>
2018-02-17 12:00:17 +0800

    all in one

---
+++ blockchain/hyperledger/1.0.5/all-in-one.sh
@@ -0,0 +1,17 @@
+docker pull hyperledger/fabric-ca:x86_64-1.0.5
+docker tag hyperledger/fabric-ca:x86_64-1.0.5 hyperledger/fabric-ca
+
+docker pull hyperledger/fabric-orderer:x86_64-1.0.5
+docker tag hyperledger/fabric-orderer:x86_64-1.0.5 hyperledger/fabric-orderer
+
+docker pull hyperledger/fabric-peer:x86_64-1.0.5
+docker tag hyperledger/fabric-peer:x86_64-1.0.5 hyperledger/fabric-peer
+
+docker pull hyperledger/fabric-couchdb:x86_64-1.0.5
+docker tag hyperledger/fabric-couchdb:x86_64-1.0.5 hyperledger/fabric-couchdb
+
+docker pull hyperledger/fabric-tools:x86_64-1.0.5
+docker tag hyperledger/fabric-tools:x86_64-1.0.5 hyperledger/fabric-tools    
+
+docker pull hyperledger/fabric-ccenv:x86_64-1.0.5
+docker tag hyperledger/fabric-ccenv:x86_64-1.0.5 hyperledger/fabric-ccenv
