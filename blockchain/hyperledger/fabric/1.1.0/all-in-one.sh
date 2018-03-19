edf64c674aeb5716ea4e4dea4658cfc53503b7f4
Neo Chan <netkiller@msn.com>
2018-03-19 13:10:21 +0800

    fabric

---
+++ blockchain/hyperledger/fabric/1.1.0/all-in-one.sh
@@ -0,0 +1,6 @@
+
+curl -s https://raw.githubusercontent.com/oscm/shell/master/blockchain/hyperledger/fabric/1.1.0/fabric-ca.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/blockchain/hyperledger/fabric/1.1.0/fabric-couchdb.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/blockchain/hyperledger/fabric/1.1.0/fabric-orderer.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/blockchain/hyperledger/fabric/1.1.0/fabric-peer.sh | bash
+curl -s https://raw.githubusercontent.com/oscm/shell/master/blockchain/hyperledger/fabric/1.1.0/fabric-tools.sh | bash
