6cd88c9f3edcd36989e6eab58bb41418f23e3e4f
Neo Chan <netkiller@msn.com>
2018-04-20 11:08:00 +0800

    Update mysql-connector-java.sh

--- database/mysql/8.0/mysql-connector-java.sh
+++ database/mysql/8.0/mysql-connector-java.sh
@@ -2,7 +2,7 @@
 
 if [ -z $(rpm -qa mysql80-community-release) ]; then
 
-    https://raw.githubusercontent.com/oscm/shell/master/database/mysql/8.0/mysql80-community-release.sh
+    curl -s https://raw.githubusercontent.com/oscm/shell/master/database/mysql/8.0/mysql80-community-release.sh | bash
 
 fi
 
