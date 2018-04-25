e3ac29fdfe44c2e4bc70280ea665e313cdab2f48
Neo Chan <netkiller@msn.com>
2018-04-25 09:49:56 +0800

    Update node-v9.11.1.sh

--- lang/node.js/binrary/node-v9.11.1.sh
+++ lang/node.js/binrary/node-v9.11.1.sh
@@ -1,5 +1,5 @@
 cd /usr/local/src
-wget wget https://nodejs.org/dist/v9.11.1/node-v9.11.1-linux-x64.tar.xz
+wget https://nodejs.org/dist/v9.11.1/node-v9.11.1-linux-x64.tar.xz
 tar xf node-v9.11.1-linux-x64.tar.xz
 mv node-v9.11.1-linux-x64 /srv/node-v9.11.1
 rm -f /srv/node
