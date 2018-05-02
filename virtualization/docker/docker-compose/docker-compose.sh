857984354aef80f4c7ac097fbe7b647d36c1cb55
Neo Chan <netkiller@msn.com>
2018-05-02 09:33:30 +0800

    Update docker-compose.sh

--- virtualization/docker/docker-compose/docker-compose.sh
+++ virtualization/docker/docker-compose/docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -sL https://github.com/docker/compose/releases/download/1.21.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
