09b8ade5ca7afbf20697442d90069190301c1373
Neo Chan <netkiller@msn.com>
2018-05-11 15:59:31 +0800

    private net

--- blockchain/ethereum/centos/README.md
+++ blockchain/ethereum/centos/README.md
@@ -10,3 +10,6 @@ Ethereum for CentOS
 	curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/golang/golang-1.10.2.sh | bash
 	curl -s https://raw.githubusercontent.com/oscm/shell/master/blockchain/ethereum/centos/go-ethereum-1.8.7.sh | bash
 	curl -s https://raw.githubusercontent.com/oscm/shell/master/blockchain/ethereum/systemd/private.sh | bash
+	
+	curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/node.js/binrary/node-v10.1.0.sh | bash
+	curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/node.js/binrary/profile.d.sh | bash
--- blockchain/ethereum/systemd/private.sh
+++ blockchain/ethereum/systemd/private.sh
@@ -1,5 +1,5 @@
 cat > /etc/sysconfig/ethereum <<'EOF'
-NETWORKID=4444
+NETWORKID=444444
 RPCADDR=0.0.0.0
 EOF
 
@@ -7,4 +7,28 @@ wget -q https://raw.githubusercontent.com/oscm/shell/master/blockchain/ethereum/
 
 systemctl daemon-reload
 systemctl enable private
+
+cat > /home/ethereum/genesis.json <<'EOF'
+{
+  "nonce": "0x0000000000000042",
+  "difficulty": "0x020000",
+  "mixhash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "coinbase": "0x0000000000000000000000000000000000000000",
+  "timestamp": "0x00",
+  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "extraData": "0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa",
+  "gasLimit": "0x4c4b40",
+  "config": {
+      "chainId": 15,
+      "homesteadBlock": 0,
+      "eip155Block": 0,
+      "eip158Block": 0
+  },
+  "alloc": { }
+}
+EOF
+
+su - ethereum -c "geth init genesis.json"
+su - ethereum -c "geth account new"
+
 systemctl start private
