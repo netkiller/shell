d777798f9e4e0b8f47d1d9409c70b1c660f94ae1
Neo Chan <netkiller@msn.com>
2018-05-11 16:01:32 +0800

    node

---
+++ lang/node.js/binrary/node.sh
@@ -0,0 +1,2 @@
+	curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/node.js/binrary/node-v10.1.0.sh | bash
+	curl -s https://raw.githubusercontent.com/oscm/shell/master/lang/node.js/binrary/profile.d.sh | bash
