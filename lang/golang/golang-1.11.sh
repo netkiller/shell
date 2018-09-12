6975031ab33801fcb4d562611436aef0258e8fbe
Neo Chan <netkiller@msn.com>
2018-09-12 17:43:48 +0800

    go 1.11

---
+++ lang/golang/golang-1.11.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Linux is " $(getconf LONG_BIT) "bit."
+
+cd /usr/local/src
+wget https://storage.googleapis.com/golang/go1.11.linux-amd64.tar.gz
+tar zxf go1.11.linux-amd64.tar.gz
+mv go /srv/go-1.11
+rm -f /srv/go
+ln -s /srv/go-1.11 /srv/go
+
+#cat >> /etc/man.config <<EOF
+#MANPATH  /srv/go/man
+#EOF
+
+cat >> /etc/profile.d/go.sh <<'EOF'
+export GOROOT=/srv/go
+export PATH=$PATH:$GOROOT/bin
+EOF
+
+source /etc/profile.d/go.sh
