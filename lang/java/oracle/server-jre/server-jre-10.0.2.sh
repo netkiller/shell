294f9986893788f761ca03abd9dc4cee91522b3a
Neo Chan <netkiller@msn.com>
2018-09-26 16:48:55 +0800

    jre

--- lang/java/oracle/server-jre/server-jre-10.0.2.sh
+++ lang/java/oracle/server-jre/server-jre-10.0.2.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cd /usr/local/src/
-#wget http://download.oracle.com/otn-pub/java/jdk/8u40-b26/server-jre-8u40-linux-x64.tar.gz?AuthParam=1427703756_960786b5c2b7cb90e20758efaec150da
+wget http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/serverjre-10.0.2_linux-x64_bin.tar.gz?AuthParam=1537857961_d1cacae4e6a9a9c244f2daad158fdd27
 tar xf serverjre-10.0.2_linux-x64_bin.tar*
 mv jdk-10.0.2 /srv/
 ln -s /srv/jdk-10.0.2 /srv/java
+
+#sed -i '117s/securerandom.source/#securerandom.source/' /srv/java/conf/security/java.security
+#sed -i '118isecurerandom.source=file:/dev/./urandom' /srv/java/conf/security/java.security
+sed -i '150s|/dev/random|/dev/urandom|' /srv/java/conf/security/java.security
 
 cat >> /etc/profile.d/java.sh <<'EOF'
 export JAVA_HOME=/srv/java
@@ -13,9 +17,7 @@ export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:.
 export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:
 EOF
 
-#sed -i '117s/securerandom.source/#securerandom.source/' /srv/java/conf/security/java.security
-#sed -i '118isecurerandom.source=file:/dev/./urandom' /srv/java/conf/security/java.security
-sed -i '150s|/dev/random|/dev/urandom|' /srv/java/conf/security/java.security
+source /etc/profile.d/java.sh
 
 cat >> /etc/man.config <<EOF
 MANPATH  /srv/java/man
