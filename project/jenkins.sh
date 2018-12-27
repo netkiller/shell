a4b6910c89bd15a04e6a943f9d06d7e0928de0ae
Neo Chan <netkiller@msn.com>
2018-12-27 15:33:08 +0800

    jenkins

--- project/jenkins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
-
-yum install jenkins
---
+++ project/jenkins/jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+yum install -y jenkins
+
+systemctl enable jenkins.service
+systemctl start jenkins.service
+
+# /etc/sysconfig/jenkins
