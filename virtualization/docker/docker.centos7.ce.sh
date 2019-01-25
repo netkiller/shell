b6b12b8565f493388152340fb6ad00bf8f0c5623
Neo Chan <netkiller@msn.com>
2019-01-25 17:22:41 +0800

    docker

--- virtualization/docker/docker.centos7.ce.sh
+++ virtualization/docker/docker.centos7.ce.sh
@@ -2,8 +2,11 @@
 
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
 yum makecache fast
+
 yum -y install docker-ce
+yum list docker-ce
 
 systemctl enable docker
 systemctl start docker
