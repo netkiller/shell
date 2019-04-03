14fde591c2521063a1c6c02ffce86ea9ac639d51
Neo Chan <netkiller@msn.com>
2019-04-03 13:50:38 +0800

    postgresql 11

---
+++ database/postgresql/postgresql11.sh
@@ -0,0 +1,2 @@
+yum install https://download.postgresql.org/pub/repos/yum/11/redhat/rhel-7-x86_64/pgdg-centos11-11-2.noarch.rpm
+yum install postgresql11
---
+++ spring/consol.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://releases.hashicorp.com/consul/1.4.4/consul_1.4.4_linux_amd64.zip
+unzip consul_1.4.4_linux_amd64.zip
+mv consul /usr/local/bin/
