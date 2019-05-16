0262e3ae1898af6037122c8331737c43d186e3b1
Neo Chan <netkiller@msn.com>
2019-05-16 19:42:53 +0800

    调整目录结构

--- os/personalise.sh
+++ os/personalise.sh
@@ -4,7 +4,7 @@ curl -s https://raw.githubusercontent.com/oscm/shell/master/os/etc.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/selinux.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/sysctl.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/ulimit.sh | bash
-curl -s https://raw.githubusercontent.com/oscm/shell/master/os/iptables/iptables.sh | bash
+#curl -s https://raw.githubusercontent.com/oscm/shell/master/os/iptables/iptables.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/ntpd/ntpdate.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/ssh/sshd_config.sh | bash
 curl -s https://raw.githubusercontent.com/oscm/shell/master/os/zmodem.sh | bash
