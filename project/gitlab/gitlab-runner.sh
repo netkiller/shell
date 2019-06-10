21d914305c1ac7cd712a9a0ee0fe4ec513bf2337
Neo Chan <netkiller@msn.com>
2019-06-10 15:56:29 +0800

    Update gitlab-runner.sh

--- project/gitlab/gitlab-runner.sh
+++ project/gitlab/gitlab-runner.sh
@@ -4,3 +4,12 @@ chmod +x /usr/local/bin/gitlab-runner
 useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 gitlab-runner start
+
+mkdir -p /home/gitlab-runner/.ssh/
+cat > /home/gitlab-runner/.ssh/config <<'EOF'
+Host *
+	StrictHostKeyChecking no
+	UserKnownHostsFile=/dev/null
+EOF
+
+chmod 600 -R /home/gitlab-runner/.ssh
