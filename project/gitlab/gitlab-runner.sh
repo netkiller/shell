670545332c2214c9e41db48fdcc79e48ce709f01
Neo Chan <netkiller@msn.com>
2019-06-10 14:56:26 +0800

    Update gitlab-runner.sh

--- project/gitlab/gitlab-runner.sh
+++ project/gitlab/gitlab-runner.sh
@@ -3,5 +3,4 @@ wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaw
 chmod +x /usr/local/bin/gitlab-runner
 useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
-gitlab-runner register
 gitlab-runner start
