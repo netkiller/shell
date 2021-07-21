8bd44ac48aa34cd1f6f260065d9f8b37432c0b2e
neo <netkiller@msn.com>
2021-07-21 14:52:25 +0800

    redis

--- database/redis/redis.sh
+++ database/redis/redis.sh
@@ -3,11 +3,18 @@
 dnf install redis -y
 
 cp /etc/redis.conf{,.original}
-sed -i 's/daemonize no/daemonize yes/' /etc/redis.conf 
-# sed -i 's/timeout 0/timeout 30/' /etc/redis.conf
-sed -i 's/tcp-keepalive 0/tcp-keepalive 60/' /etc/redis.conf 
-sed -i 's/# maxclients 10000/maxclients 10000/' /etc/redis.conf
-sed -i 's/# maxmemory-policy noeviction/maxmemory-policy volatile-lru/g' /usr/local/etc/redis.conf
+#sed -i 's/daemonize no/daemonize yes/' /etc/redis.conf 
+sed -i 's/timeout 0/timeout 30/' /etc/redis.conf
+#sed -i 's/tcp-keepalive 0/tcp-keepalive 60/' /etc/redis.conf 
+sed -i 's/# maxclients 10000/maxclients 1000/' /etc/redis.conf
+sed -i 's/# maxmemory-policy noeviction/maxmemory-policy volatile-lru/g' /etc/redis.conf
+
+
+cat >>  /etc/security/limits.d/20-nofile.conf <<EOF
+
+redis soft nofile 65535
+redis hard nofile 65535
+EOF
 
 
 cat >> /etc/sysctl.conf <<EOF
