7b33dcf4f09c25ee59e15bfe13b86c74c9e1b51b
Neo Chen <netkiller@msn.com>
2021-12-08 19:13:53 +0800

    rocky linux

---
+++ os/rocky.sh
@@ -0,0 +1,55 @@
+dnf -y upgrade
+dnf -y install epel-release
+
+dnf install -y bzip2 tree psmisc \
+telnet wget rsync vim-enhanced \
+net-tools bind-utils
+
+timedatectl set-timezone Asia/Shanghai	
+dnf install -y langpacks-en glibc-langpack-en
+localectl set-locale LANG=en_US.UTF-8
+
+cat >> /etc/environment <<EOF
+LC_ALL=en_US.UTF-8
+LANG=en_US.UTF-8
+LC_CTYPE=UTF-8
+EOF
+
+cat >> /etc/profile.d/history.sh <<EOF
+# Administrator specific aliases and functions for system security
+export HISTSIZE=10000
+export HISTFILESIZE=10000
+export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
+export TIME_STYLE=long-iso
+EOF
+source /etc/profile.d/history.sh
+
+cp /etc/selinux/config{,.original}
+sed -i "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
+setenforce Permissive
+
+cat >> /etc/sysctl.conf <<EOF
+
+# Netkiller
+net.ipv4.ip_local_port_range = 1025 65500
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_keepalive_time = 1800
+net.core.netdev_max_backlog=3000
+net.ipv4.tcp_max_syn_backlog = 1024
+net.ipv4.tcp_max_tw_buckets = 4096
+net.core.somaxconn = 1024
+
+# TCP BBR
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+sysctl -p
+
+cat > /etc/security/limits.d/20-nofile.conf <<EOF
+
+* soft nofile 65535
+* hard nofile 65535
+
+EOF
+
--- virtualization/docker/docker.install.sh
+++ virtualization/docker/docker.install.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-if [ -z "$( egrep "CentOS|Redhat" /etc/centos-release)" ]; then
-        echo 'Only for Redhat or CentOS'
-        exit
-fi
-
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-
 dnf install -y docker-ce
 
 systemctl enable docker
 systemctl start docker
 
 # usermod -aG docker $USER
+adduser -u 992 -g 992 docker
+#usermod -aG docker docker && newgrp docker
