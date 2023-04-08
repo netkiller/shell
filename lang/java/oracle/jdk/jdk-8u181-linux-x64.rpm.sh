8b9977ccb4d57ea9ffc39daa22bacf1a717e0d98
Neo Chan <netkiller@msn.com>
2023-04-08 17:23:40 +0800

    modified:   lang/java/oracle/jdk/jdk-8u181-linux-x64.rpm.sh

--- lang/java/oracle/jdk/jdk-8u181-linux-x64.rpm.sh
+++ lang/java/oracle/jdk/jdk-8u181-linux-x64.rpm.sh
@@ -8,3 +8,6 @@ dnf localinstall -y jdk-8u181-linux-x64.rpm
 
 rm -f /srv/java
 ln -s /usr/java/default /srv/java
+
+# alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_361-amd64/jre/bin/java 0
+# alternatives --install /usr/bin/javac javac /usr/java/jdk1.8.0_361-amd64/bin/javac 0
