0a9ac9a58ec822d8e3b6c999b926e6cefc4649b1
netkiller <netkiller@msn.com>
2026-04-08 19:30:47 +0800

    0.7.18 - 优化配置文件

--- virtualization/docker/docker.install.sh
+++ virtualization/docker/docker.install.sh
@@ -3,12 +3,16 @@ dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce
 dnf install -y docker-ce docker-compose-plugin
 
 systemctl enable docker
-systemctl start 
+systemctl start docker
 
 dnf list installed | grep docker
 
 GID=$(egrep -o 'docker:x:([0-9]+)' /etc/group | egrep -o '([0-9]+)')
-adduser -u ${GID} -g ${GID} docker
+adduser --system -u ${GID} -g ${GID} -G wheel -c "Container Administrator" docker
 
 #adduser -u 992 -g 992 docker
 #usermod -aG docker docker && newgrp docker
+
+cat > /etc/sudoers.d/docker <<-EOF
+docker    ALL=(ALL)    NOPASSWD: ALL
+EOF
